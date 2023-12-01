6b41e8bd814dd2fc3006a718a8d08facd17134ab
Bren2024 <bnakata24@punahou.edu>
2023-12-01 09:36:14 -1000

    Fix piece visibility while dragging

--- chess_board.py
+++ chess_board.py
@@ -19,20 +19,27 @@ pieces = {}
 for piece in (["b"+i for i in chess.PIECE_SYMBOLS[1:]] + ["w"+i for i in chess.PIECE_SYMBOLS[1:]]):
     if (piece == None):
         continue
-    print()
     img = pygame.image.load(f"images/{piece}.png")
     img = pygame.transform.scale(img, (SQUARE_SIZE, SQUARE_SIZE))
     pieces[piece] = img
 
-def draw_board(screen, board):
+def get_piece_symbol(color, symbol):
+    return f"{'w' if color else 'b'}{symbol.lower()}"
+
+def draw_board(screen, board, dragging_piece=None):
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE):
             color = LIGHT_COLOR if (row + col) % 2 == 0 else DARK_COLOR
             pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
             piece = board.piece_at(chess.square(col, BOARD_SIZE - 1 - row))
-            if piece:
-                screen.blit(pieces[("w" if str(piece.symbol()).isupper() else "b") + piece.symbol().lower()], (col * SQUARE_SIZE, row * SQUARE_SIZE))
+            if piece and (True if dragging_piece == None else dragging_piece['square'] != (7-row)*8+col):
+                symbol = get_piece_symbol(piece.color, piece.symbol())
+                screen.blit(pieces[symbol], (col * SQUARE_SIZE, row * SQUARE_SIZE))
+
+    if dragging_piece:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        screen.blit(pieces[dragging_piece['piece']], (mouse_x - SQUARE_SIZE // 2, mouse_y - SQUARE_SIZE // 2))
 
 def get_square_from_mouse(pos):
     col = pos[0] // SQUARE_SIZE
@@ -46,7 +53,8 @@ def play_game():
     board = chess.Board()
     clock = pygame.time.Clock()
 
-    startSquare = None
+    start_square = None
+    dragging_piece = None
 
     running = True
     while running and not board.is_game_over():
@@ -54,16 +62,20 @@ def play_game():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                startSquare = get_square_from_mouse(pygame.mouse.get_pos())
+                start_square = get_square_from_mouse(pygame.mouse.get_pos())
+                piece = board.piece_at(start_square)
+                if piece:
+                    dragging_piece = {"piece" : get_piece_symbol(piece.color, piece.symbol()), "square" : start_square}
             elif event.type == pygame.MOUSEBUTTONUP:
-                endSquare = get_square_from_mouse(pygame.mouse.get_pos())
-                moves = [move for move in board.legal_moves if move.from_square == startSquare and move.to_square == endSquare]
+                end_square = get_square_from_mouse(pygame.mouse.get_pos())
+                moves = [move for move in board.legal_moves if move.from_square == start_square and move.to_square == end_square]
                 if moves:
                     move = moves[0]
                     board.push(move)
+                dragging_piece = None
 
         screen.fill((255, 255, 255))
-        draw_board(screen, board)
+        draw_board(screen, board, dragging_piece)
         pygame.display.flip()
         clock.tick(60)
 
@@ -71,3 +83,4 @@ def play_game():
 
 if __name__ == "__main__":
     play_game()
+    pygame.quit()
